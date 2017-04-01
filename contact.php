b41aadea0f774b87645f19983338f8e09e511128
Stuart Olivera <stuart@stuartolivera.com>
2017-04-01 19:32:26 -0400

    Use sparkpost for email form

--- contact.php
+++ contact.php
@@ -17,6 +17,35 @@ if (trim($subject) != '')
   $content .= "Subject:  " . $subject . "\r\n";
 $content .=  "Message:  \r\n" . $message . "\r\n----";
 
-mail('stuart@oliveraweb.com', 'Contact From ' . $_SERVER['HTTP_HOST'], $content);
+
+require 'vendor/autoload.php';
+
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+
+$httpClient = new GuzzleAdapter(new Client());
+$sparky = new SparkPost($httpClient, ['key'=>$_ENV['SPARKPOST_KEY']]);
+$sparky->setOptions(['async' => false]);
+
+$promise = $sparky->transmissions->post([
+    'content' => [
+        'from' => [
+            'name' => 'Olivera Web',
+            'email' => 'hello@oliveraweb.com',
+        ],
+        'subject' => 'Contact From ' . $_SERVER['HTTP_HOST'],
+        'text' => $content,
+    ],
+    'substitution_data' => ['name' => 'YOUR_FIRST_NAME'],
+    'recipients' => [
+        [
+            'address' => [
+                'name' => 'Stuart Olivera',
+                'email' => 'stuart@oliveraweb.com',
+            ],
+        ],
+    ],
+]);
 
 ?>
